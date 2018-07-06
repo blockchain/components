5f7c94353ba80d32ee37c8798056edb11888df73
Méril <merildev@users.noreply.github.com>
2018-07-06 14:42:50 +0100

    chore(Package): Remove logic from package.json and fix test coverage (#5)

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+const preprocessMock = require.resolve('./utils/preprocessor')
+const enzyme = require.resolve('./utils/enzyme')
+const raf = require.resolve('./utils/raf')
+
+const assetExt = ['css', 'eot', 'gif', 'jpeg', 'jpg', 'png', 'svg', 'ttf']
+
+const codeExt = ['js']
+
+const getTransformString = (extensions) =>
+  extensions
+    .reduce((acc, val) => `${acc}^.+\\\\*${val}|`, '')
+    .replace(/.$/, '$')
+
+const coverage =
+  process.argv.indexOf('--coverage') !== -1
+    ? {
+        collectCoverage: true,
+        collectCoverageFrom: [
+          'src/**/**.js',
+          '!src/Tools/**',
+          '!**/__snapshots__/**',
+          '!**/**.stories.js',
+          '!**/index.js',
+        ],
+        coverageDirectory: './coverage',
+      }
+    : {}
+
+module.exports = {
+  ...coverage,
+  clearMocks: true,
+  moduleFileExtensions: [codeExt],
+  rootDir: process.cwd(),
+  setupFiles: [raf, enzyme],
+  snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
+  testPathIgnorePatterns: ['/node_modules/', '.*.ignore.spec.*'],
+  transform: {
+    [getTransformString(codeExt)]: 'babel-jest',
+    [getTransformString(assetExt)]: preprocessMock,
+  },
+  transformIgnorePatterns: ['node_modules/'],
+}
