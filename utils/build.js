c5ca19bc62c125f1052a54611888004fee1a0be0
Méril <merildev@users.noreply.github.com>
2018-07-10 11:43:50 +0100

    chore(Build): Fix building task (#10)

--- utils/build.js
+++ utils/build.js
@@ -1,22 +1,15 @@
 const { execSync } = require('child_process')
-const { inInstall } = require('in-publish')
 const path = require('path')
-const rimraf = require('rimraf')
-const { promisify } = require('util')
 
-if (inInstall()) {
-  process.exit(0)
-}
-
-const rootDir = path.resolve(__dirname, '../')
-const babelPath = path
+const babel = path
   .resolve(__dirname, '../node_modules/.bin/babel')
   .replace(/ /g, '\\ ')
-const rimrafAsync = promisify(rimraf)
 
-const exec = (command, extraEnv) =>
+const babelrc = path.resolve(__dirname, '../.babelrc').replace(/ /g, '\\ ')
+
+const exec = (command) =>
   execSync(command, {
-    env: Object.assign({}, process.env, extraEnv),
+    env: process.env,
     stdio: 'inherit',
   })
 
@@ -26,16 +19,7 @@ const ignoreGlobs = [
   '**/__snapshots__',
 ].join(',')
 
-console.log('Deleting old build folders...') // eslint-disable-line no-console
+const baseCmd = `${babel} src/ --config-file ${babelrc} -D --ignore "${ignoreGlobs}"`
 
-Promise.all([rimrafAsync(`${rootDir}/esm`), rimrafAsync(`${rootDir}/lib`)])
-  .then(() => {
-    exec(`${babelPath} src/ -d esm/ --copy-files --ignore "${ignoreGlobs}"`)
-    exec(
-      `${babelPath} src/ -d lib/ --copy-files --plugins @babel/plugin-transform-modules-commonjs --ignore "${ignoreGlobs}"`,
-    )
-  })
-  .catch((error) => {
-    console.error('One of the commands failed:', error.stack) // eslint-disable-line no-console
-    process.exit(1)
-  })
+exec(`${baseCmd} -d lib/ --plugins @babel/plugin-transform-modules-commonjs`)
+exec(`${baseCmd} -d esm/`)
