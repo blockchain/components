565426170b22d4c4142cb98522fbdea8a5ff4b9c
Meril Pilon <meril@blockchain.com>
2018-07-16 11:41:45 +0100

    test(button): improve coverage and use deep rendering

--- src/Atoms/Buttons/Button.spec.js
+++ src/Atoms/Buttons/Button.spec.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
+import { mount } from 'enzyme'
 
+import theme from '../../Tools/theme'
 import Button from './Button'
 
 describe('Button component', () => {
-  it('renders correctly', () => {
-    const component = shallow(<Button />)
-    const tree = toJson(component)
+  it('should render correctly', () => {
+    const tree = mount(<Button theme={theme} />)
+
     expect(tree).toMatchSnapshot()
+  })
+
+  it('should have a disabled state', () => {
+    const tree = mount(<Button disabled theme={theme} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should have a secondary style', () => {
+    const tree = mount(<Button nature="secondary" theme={theme} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should have a type property', () => {
+    const submit = mount(<Button theme={theme} type="submit" />)
+
+    expect(submit).toMatchSnapshot()
+
+    const reset = mount(<Button theme={theme} type="reset" />)
+
+    expect(reset).toMatchSnapshot()
   })
 })
