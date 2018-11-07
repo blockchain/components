3eb95445d253147a33c672b6554a487197eb3e7a
Terence Buchannan <39777266+tbuchann@users.noreply.github.com>
2018-11-07 06:22:52 -0500

    feat(input): add radio functionality (#162)
    - Add radio capability to the Input Component

--- src/Atoms/Inputs/Input.stories.js
+++ src/Atoms/Inputs/Input.stories.js
@@ -8,5 +8,11 @@ storiesOf('Atoms/Inputs/Input', module)
   .add('with checkbox', () => (
     <Input label="Checkbox" name="test" type="checkbox" />
   ))
+  .add('with radio', () => (
+    <div>
+      <Input label="Bitcoin" name="test" type="radio" value="bitcoin" />
+      <Input label="Ethereum" name="test" type="radio" value="ethereum" />
+    </div>
+  ))
   .add('with width', () => <Input width="300px" />)
   .add('with disabled', () => <Input disabled />)
