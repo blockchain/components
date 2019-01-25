c4d270f90ada10eda74068f48d5f477242d50a33
Lyncee <guillaume@blockchain.com>
2019-01-25 11:51:13 +0000

    feat(palette): add story

---
+++ src/Tools/palette.stories.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import styled from 'styled-components'
+import { parseToRgb } from 'polished'
+import { equals, keysIn, sum, values } from 'ramda'
+
+import palette from './palette'
+
+const thresholdColor = Math.trunc((255 * 3) / 2)
+
+const selectReadableColor = (color) => {
+  const colorValue = palette[color]
+  return equals('transparent', color)
+    ? 'black'
+    : sum(values(parseToRgb(colorValue))) < thresholdColor
+    ? 'white'
+    : 'black'
+}
+
+const selectColors = (color) => ({
+  background: palette[color],
+  foreground: selectReadableColor(color),
+})
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  width: 100%;
+
+  & > :not(:last-child) {
+    margin-right: 0.5rem;
+  }
+
+  > * {
+    margin-bottom: 10px;
+    box-sizing: border-box;
+  }
+`
+const Cell = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(10% - 0.5rem);
+  padding: 2rem 1rem;
+  box-sizing: border-box;
+  background: ${(props) => props.background};
+  color: ${(props) => props.foreground};
+
+  & > :first-child {
+    margin-bottom: 0.5rem;
+    box-sizing: border-box;
+  }
+`
+const ColorKey = styled.span`
+  font-size: 1rem;
+  font-weight: 700;
+`
+const ColorHex = styled.span`
+  font-size: 0.7rem;
+  font-weight: 400;
+`
+
+const Palette = () => (
+  <Wrapper>
+    {keysIn(palette).map((color) => (
+      <Cell {...selectColors(color)} key={color}>
+        <ColorKey>{color}</ColorKey>
+        <ColorHex>{palette[color]}</ColorHex>
+      </Cell>
+    ))}
+  </Wrapper>
+)
+
+storiesOf('palette', module).add('default', () => <Palette />)
